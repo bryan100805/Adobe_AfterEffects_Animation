1e0276c72e8f46351fe3a59e0b85c0efb96dfd9f
bryan100805 <bryantan100895@gmail.com>
2024-05-20 16:28:02 +0800

    Updates

---
+++ Tweening/Flubber/quadratic_bezier_interpolation/downloadSVG.js
@@ -0,0 +1,29 @@
+function downloadFrames(pathFrames, numFrames, startPaths) {
+    const downloadLinksContainer = document.getElementById('downloadLinks');
+    downloadLinksContainer.innerHTML = '';
+
+    for (let frameIndex = 0; frameIndex <= numFrames; frameIndex++) {
+        const frameSvg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        frameSvg.setAttribute("viewBox", "0 0 1920 1080");
+        frameSvg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+
+        startPaths.forEach((path, index) => {
+            const newPath = document.createElementNS("http://www.w3.org/2000/svg", "path");
+            newPath.setAttribute("d", pathFrames[index][frameIndex]);
+            newPath.setAttribute("fill", path.getAttribute("fill")); // Use the same fill color as the original path
+            frameSvg.appendChild(newPath);
+        });
+
+        const serializer = new XMLSerializer();
+        const svgBlob = new Blob([serializer.serializeToString(frameSvg)], { type: "image/svg+xml" });
+        const url = URL.createObjectURL(svgBlob);
+
+        const link = document.createElement("a");
+        link.href = url;
+        link.download = `frame_${frameIndex+1}.svg`;
+        link.textContent = `Download Frame ${frameIndex+1}`;
+        link.className = "download-link";
+        link.style.display = "inline-block";
+        downloadLinksContainer.appendChild(link); 
+    }
+}
